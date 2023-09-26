e3cb577d18170d855a9bac11480cb1edf293eaee
Alfie213 <arturbogoratov@gmail.com>
2023-09-26 20:46:12 +0300

    HoldingButton

---
+++ Assets/HoldingButton.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+// [RequireComponent(typeof())]
+public class HoldingButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private UnityEvent onHolding;
+
+    private bool holdingButton;
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        holdingButton = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        holdingButton = false;
+    }
+
+    private void Update()
+    {
+        if (holdingButton)
+            onHolding.Invoke();
+    }
+}
